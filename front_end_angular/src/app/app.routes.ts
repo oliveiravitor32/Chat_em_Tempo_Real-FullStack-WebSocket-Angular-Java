cb1cb9eac7d8f561dc598d497b717c94c366d38c
oliveiravitor32 <oliveiravitor327@gmail.com>
2024-09-23 14:32:20 -0300

    Cria componente de página para rotas não encontradas.

--- front_end_angular/src/app/app.routes.ts
+++ front_end_angular/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 import { ChatComponent } from './pages/chat/chat.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: RegisterComponent },
   { path: 'chat', component: ChatComponent, canActivate: [authGuard] },
+  { path: '**', component: NotFoundComponent },
 ];
