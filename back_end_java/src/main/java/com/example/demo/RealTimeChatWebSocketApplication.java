efcc2ad26d724b5092cd0f3415eac4c56c01b3b7
oliveiravitor32 <oliveiravitor327@gmail.com>
2024-09-23 14:54:21 -0300

    Renomeia classe principal do WebSocket

--- back_end_java/src/main/java/com/example/demo/RealTimeChatWebSocketApplication.java
+++ back_end_java/src/main/java/com/example/demo/RealTimeChatWebSocketApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebSocketTrainingApplication {
+public class RealTimeChatWebSocketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebSocketTrainingApplication.class, args);
+		SpringApplication.run(RealTimeChatWebSocketApplication.class, args);
 	}
 
 }
--- back_end_java/src/test/java/com/example/demo/RealTimeChatWebSocketApplicationTests.java
+++ back_end_java/src/test/java/com/example/demo/RealTimeChatWebSocketApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebSocketTrainingApplicationTests {
+class RealTimeChatWebSocketApplicationTests {
 
 	@Test
 	void contextLoads() {
